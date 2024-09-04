af9685ee91cb83bf9bbb2515505ad87a542926d7
ShiroHayabusa <shirohayabbbusa@gmail.com>
2024-09-04 16:17:27 -0400

    9/4/24 - 1

--- src/catalog/trims/ViewTrim.js
+++ src/catalog/trims/ViewTrim.js
@@ -6,7 +6,24 @@ export default function ViewTrim() {
 
     const [trim, setTrim] = useState({
         name: '',
-        bodystyle: ''
+        bodystyle: '',
+        description: '',
+        engine: '',
+        transmission: '',
+        body: '',
+        drivetrain: '',
+        years: '',
+        tuner: '',
+        amount: '',
+        maxSpeed: '',
+        acceleration: '',
+        weight: '',
+        uniq: '',
+        electric: '',
+        hybrid: '',
+        battery: '',
+        range: '',
+        market: ''
     });
 
     const { make, model, generationId, bodystyleId, trimId } = useParams();
@@ -32,8 +49,6 @@ export default function ViewTrim() {
             </ul>
 
             <div className='container'>
-
-
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="/" className="text-decoration-none">Home</a></li>
@@ -45,38 +60,75 @@ export default function ViewTrim() {
                         <li class="breadcrumb-item active" aria-current="page">{trim.name}</li>
                     </ol>
                 </nav>
+
                 <div className="h5 pb-1 mb-4 text-black border-bottom border-black text-start">
                     {make + ' ' + model + ' ' + trim.name}
                 </div>
 
-                <div className="row">
-                    <div className="col-lg-4 border">
-                        <div>
-                            Year
-                        </div>
-                        <div>
-                            Description
-                        </div>
-                    </div>
-                    <div className="col-lg-8">
-                        <ul className="nav nav-tabs">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="#">Specs</a>
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+                    <div className="col">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item text-start">
+                                Years of production: {trim.years}
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="#">Photo</a>
-                                <img src={`https://newloripinbucket.s3.amazonaws.com/image/catalog/${make || 'defaultMake'}/${model || 'defaultModel'}/${trim.bodystyle.generation?.name || 'defaultGeneration'}/${trim.bodystyle?.facelift?.name || 'defaultBodystyle'}/${trim?.bodystyle?.bodytype?.name}/${trim.name}/${trim?.photo?.name || 'defaultImage.jpg'}`} className="img-thumbnail"></img>
+                            <li class="list-group-item text-start">
+                                Market: {trim.market?.name}
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Video</a>
+                            <li class="list-group-item text-start">
+                                Amount: {trim.amount}
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled">Spots</a>
+                            <li class="list-group-item text-start">
+                                {trim.description}
+                            </li>
+                            <li class="list-group-item text-start">
+                                Engine: <Link to={`/administration/engines/${make}/${trim.engine?.id}`} className="text-decoration-none">{trim.engine?.name}</Link>
+                                <p>{trim.engine?.displacement} {trim.engine?.power} {trim.engine?.torque}</p>
+                            </li>
+                            <li class="list-group-item text-start">
+                                Transmission: <Link to={`/administration/transmissions/${make}/${trim.transmission?.id}`} className="text-decoration-none">{trim.transmission?.name}</Link>
+                                <p>{trim.transmission?.transmissionType?.name}</p>
+                            </li>
+                            <li class="list-group-item text-start">
+                                Body: {trim.body?.name}
+                            </li>
+                            <li class="list-group-item text-start">
+                                Drivetrain: {trim.drivetrain?.name}
                             </li>
                         </ul>
                     </div>
-                    <div class="col-lg-4 border">
-
+                    <div className="col">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item text-start">
+                                Max speed: {trim.maxSpeed}
+                            </li>
+                            <li class="list-group-item text-start">
+                                Acceleration: {trim.acceleration}
+                            </li>
+                            <li class="list-group-item text-start">
+                                Weight: {trim.weight}
+                            </li>
+                            <li class="list-group-item text-start">
+                                Length: {trim.bodystyle.length}
+                            </li>
+                            <li class="list-group-item text-start">
+                                Width: {trim.bodystyle.width}
+                            </li>
+                            <li class="list-group-item text-start">
+                                Height: {trim.bodystyle.height}
+                            </li>
+                            <li class="list-group-item text-start">
+                                Wheelbase: {trim.bodystyle.base}
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <img
+                            src={`https://newloripinbucket.s3.amazonaws.com/image/catalog/${make}/${model}/${trim.bodystyle.generation?.name}/${trim.bodystyle?.facelift?.name}/${trim?.bodystyle?.bodytype?.name}/${trim.name}/${trim?.photo?.name}`} className="img-thumbnail">
+                        </img>
+                    </div>
+                </div>
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2">
+                    <div class="col border">
                         <div className="mb-3">
                             <label for="exampleFormControlTextarea1" className="form-label">Comments</label>
                             <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
